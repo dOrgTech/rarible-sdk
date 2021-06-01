fb91ffa1282d69cccd61a5f29bb801759956694e
Ben Walker <ben.walker18@gmail.com>
2021-06-01 13:31:46 -0700

    Clarify possible asset types

--- src/models/commons.ts
+++ src/models/commons.ts
@@ -1,7 +1,12 @@
 import {PinataCredentials} from "./pinata";
 
 export interface AssetType {
-    assetClass: number,
+    /**
+    * This is a hash of the asset class's name.
+    * Possible class names are ETH, ERC20, ERC721, & ERC1155
+    * Example hash generation: Web3.utils.soliditySha3("ETH")
+    **/
+    assetClass: string,
     /**
     * This is generic data because the format of data is varies by asset class.
     * For example, for an asset class of ERC-20, the data holds the address of the token.
