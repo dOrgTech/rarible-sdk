1d0a35b139b88e509a5971321abf705fe51d2cad
carlos <carlosfebres97@hotmail.com>
2021-06-03 00:15:41 -0400

    format with prettier

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -1,50 +1,56 @@
-import { Asset } from './commons'
+import { Asset } from "./commons";
 
 export interface Order {
-  makerAddress: string
-  makeAsset: Asset
-  takerAddress: string
-  takeAsset: Asset
+  makerAddress: string;
+  makeAsset: Asset;
+  takerAddress: string;
+  takeAsset: Asset;
   /**
    * Random number to distinguish between a maker's orders
    **/
-  salt: number
+  salt: number;
   /**
    * Order can't be matched before this block timestamp (optional)
    **/
-  startBlockTimestamp?: number
+  startBlockTimestamp?: number;
   /**f
    * Order can't be matched after this block timestamp (optional)
    **/
-  endBlockTimestamp?: number
+  endBlockTimestamp?: number;
 }
 
 export interface OrderFilter {
-    origin: string,
-    sort?: 'LAST_UPDATE',
-    size?: number,
-    continuation?: string, 
+  origin: string;
+  sort?: "LAST_UPDATE";
+  size?: number;
+  continuation?: string;
 }
 
 export interface OrderCollectionFilter extends OrderFilter {
-  collectionAddress: string
+  collectionAddress: string;
 }
 
 export interface OrderMakerFilter extends OrderFilter {
-  makerAddress: string
+  makerAddress: string;
 }
 
 export interface OrderItemFilter extends OrderFilter {
-  contractAddress: string
-  tokenId: BigInteger
+  contractAddress: string;
+  tokenId: BigInteger;
 }
 
-export interface SearchFilter extends OrderFilter{
-    type: "sell" | "sell_by_maker" | "sell_by_item" | "sell_by_collection" | "bid_by_item" | "bid_by_maker",
-    maker?: string,
-    token?: string,
-    tokenId?: BigInteger,
-    collection?: string
+export interface SearchFilter extends OrderFilter {
+  type:
+    | "sell"
+    | "sell_by_maker"
+    | "sell_by_item"
+    | "sell_by_collection"
+    | "bid_by_item"
+    | "bid_by_maker";
+  maker?: string;
+  token?: string;
+  tokenId?: BigInteger;
+  collection?: string;
 }
 
 /**
@@ -52,8 +58,8 @@ export interface SearchFilter extends OrderFilter{
  **/
 
 export interface SellOrder extends Order {
-  type: 'RARIBLE_V1' | 'RARIBLE_V2'
-  data: SellData
+  type: "RARIBLE_V1" | "RARIBLE_V2";
+  data: SellData;
   /**
    * signature: Mentioned in API docs but
    * should be calculated under the hood
@@ -64,23 +70,23 @@ export interface SellOrder extends Order {
 
 // If supporting LEGACY, need 'fee'.
 export interface SellData {
-  dataType: 'RARIBLE_V2_DATA_V1' | 'LEGACY'
-  payouts?: Allotment[]
-  originFees?: Allotment[]
-  fee?: number
+  dataType: "RARIBLE_V2_DATA_V1" | "LEGACY";
+  payouts?: Allotment[];
+  originFees?: Allotment[];
+  fee?: number;
 }
 
 export interface Allotment {
-  account: string
-  value: number
+  account: string;
+  value: number;
 }
 
 export interface SellOrderResponse extends SellOrder {
-  fill: number
-  makeStock: number
-  cancelled: boolean
-  createdAt: string
-  lastUpdateAt: string
-  signature: string
-  hash: string
+  fill: number;
+  makeStock: number;
+  cancelled: boolean;
+  createdAt: string;
+  lastUpdateAt: string;
+  signature: string;
+  hash: string;
 }
