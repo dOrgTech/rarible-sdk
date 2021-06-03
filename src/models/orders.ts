23f14a5a5348a7fee0b4bd42c6b3b5339bcdf6a3
JT <jason.s.theobald@gmail.com>
2021-06-02 21:48:24 -0700

    Fixes

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -13,24 +13,6 @@ export interface Order {
    * Order can't be matched before this block timestamp (optional)
    **/
   startBlockTimestamp?: number;
-  /**f
-   * Order can't be matched after this block timestamp (optional)
-   **/
-  endBlockTimestamp?: number;
-
-export interface Order {
-  makerAddress: string
-  makeAsset: Asset
-  takerAddress: string
-  takeAsset: Asset
-  /**
-   * Random number to distinguish between a maker's orders
-   **/
-  salt: number
-  /**
-   * Order can't be matched before this block timestamp (optional)
-   **/
-  startBlockTimestamp?: number
   /**
    * Order can't be matched after this block timestamp (optional)
    **/
